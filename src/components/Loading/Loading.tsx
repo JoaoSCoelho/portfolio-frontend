8e1c1dbf6d06e864b5b4af7c22e0bc5c233978a6
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-07 11:42:14 -0300

    🚨

--- src/components/Loading/Loading.tsx
+++ src/components/Loading/Loading.tsx
@@ -1,11 +1,14 @@
 // eslint-disable-next-line import/named
-import { IconBaseProps } from 'react-icons';
-import { AiOutlineLoading } from 'react-icons/ai';
+import { IconBaseProps } from 'react-icons'
+import { AiOutlineLoading } from 'react-icons/ai'
 
-import styles from './Loading.module.css';
+import styles from './Loading.module.css'
 
 export default function Loading({ className, ...props }: IconBaseProps) {
   return (
-    <AiOutlineLoading className={[styles.loading, className].join(' ')} {...props} />
-  );
+    <AiOutlineLoading
+      className={[styles.loading, className].join(' ')}
+      {...props}
+    />
+  )
 }
