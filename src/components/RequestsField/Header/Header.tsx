1f71b43726228033cc8b905acc9ccbd22442cf23
João Victor Coelho <joaovictor-san-co@hotmail.com>
2023-08-07 10:03:43 -0300

    🚨

--- src/components/RequestsField/Header/Header.tsx
+++ src/components/RequestsField/Header/Header.tsx
@@ -5,12 +5,20 @@ import styles from './Header.module.css'
 export default function Header() {
   return (
     <div className={styles.header}>
-      <button className={`${styles.button} ${styles.first}`} disabled type="button">
+      <button
+        className={`${styles.button} ${styles.first}`}
+        disabled
+        type="button"
+      >
         <span className={styles.text}>No Environment</span>
         <BsFillCaretDownFill />
       </button>
 
-      <button className={`${styles.button} ${styles.last}`} disabled type="button">
+      <button
+        className={`${styles.button} ${styles.last}`}
+        disabled
+        type="button"
+      >
         Cookies
       </button>
     </div>
